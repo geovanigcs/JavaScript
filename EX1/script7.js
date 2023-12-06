210c2eb924bc40a7c9391fd3fc559b41e6bdb6bc
geovanigcs <geovanicordeiro13@gmail.com>
2023-12-06 12:08:42 -0300

    Adição de trabalhos

---
+++ EX1/script7.js
@@ -0,0 +1,8 @@
+const alunos = ['Geovani', 'Angelina', 'Matheus'];
+alunos.unshift('Luiza') //Para adicionar antes
+// ao invés de usar para adicionar no fim => alunos[alunos.length] = 'Marcelo'
+alunos.push('Michael') //use para adicionar no fim
+//const removido = alunos.shift(); //Para remover o primeiro
+//const removido = alunos.pop(); //Para remover o último
+console.log(removido);
+console.log(alunos);
