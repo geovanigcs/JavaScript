ac710bb4694454bde98cf83c81d1a8a286476818
geovanigcs <geovanicordeiro13@gmail.com>
2023-12-06 11:01:36 -0300

    Adição de resultados

--- EX1/script6.js
+++ EX1/script6.js
@@ -1,2 +1,12 @@
-const num = prompt ('Digite um número: ');
-console.log(num = 100 ** (1/2))
+const num = Number(prompt ('Digite um número: '));
+const numero = document.getElementById('numero-titulo');
+const text = document.getElementById('texto');
+numero.innerHTML = num;
+
+text.innerHTML = '';
+text.innerHTML += `<p>Raiz quadrada: ${num ** 0.5}</p>`;
+text.innerHTML += `<p>É NaN:  ${Number.isNaN(num)}</p>`;
+text.innerHTML += `<p>${num} é inteiro: ${Number.isInteger(num)}</p>`;
+text.innerHTML += `<p>Arredondando para baixo: ${Math.floor(num)}</p>`;
+text.innerHTML += `<p>Arredondando para cima: ${Math.ceil(num)}</p>`;
+text.innerHTML += `<p>Com duas casas decimais: ${num.toFixed(2)}</p>`;
