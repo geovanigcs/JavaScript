09b96f218b085a1283e1341edfbb82a8d4061678
geovanigcs <geovanicordeiro13@gmail.com>
2024-05-13 17:25:13 -0300

    Operadores de comparação e lógicos

--- EX1/script11.js
+++ EX1/script11.js
@@ -2,6 +2,22 @@
 Entre 12 - 17 = Boa tarde
 Entre 18 - 23 = Boa noite*/
 
+
+// Operadores de comparação
+// > maior que
+// >= maior que ou igual a
+// < menor que
+// <= menor que ou igual a
+// == igualdade (valor)
+// === igualdade estrita (valor e tipo(ex: string or number))
+// != diferente (valor)
+// !== diferente estrito (valor e tipo(ex: string or number))
+
+// Operadores lógicos
+// && -> AND -> E
+// || -> OR -> OU
+// ! -> NOT -> NÃO
+
 const hora = 12;
 if (hora <= 0 && hora <= 11) {
     console.log ('Bom dia');
